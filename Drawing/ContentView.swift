15ce219fbc67aa83e3917fede2a0cffe6bebf6aa
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-10 21:09:58 +0200

    Try out some ImagePaint code

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -34,7 +34,7 @@ struct ContentView: View {
 //            .strokeBorder(Color.blue, lineWidth: 40)
         VStack {
             Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                //.stroke(Color.red, lineWidth: 1)
+//                .stroke(Color.red, lineWidth: 1)
                 .fill(Color.red, style: FillStyle(eoFill: true))
             
             Text("Offset")
@@ -44,6 +44,19 @@ struct ContentView: View {
             Text("Width")
             Slider(value: $petalWidth, in: 0...100)
                 .padding(.horizontal)
+            
+            Text("Hello World")
+                .frame(width: 150, height: 150)
+//                .background(Color.red)
+//                .border(Color.red, width: 30)
+//                .background(Image("Example"))
+//                .border(Image("Example"), width: 30)
+//                .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
+                .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+            
+            Capsule()
+                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
+                .frame(width: 300, height: 200)
         }
     }
 }
