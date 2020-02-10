15440329d1aa7e69fff898414f367135291e902c
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-10 21:32:19 +0200

    Enable high-performance Metal rendering with drawingGroup()

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,8 +9,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+//    @State private var petalOffset = -20.0
+//    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
 //        Path { path in
@@ -32,32 +33,68 @@ struct ContentView: View {
 //            .strokeBorder(Color.blue, lineWidth: 40)
 //        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
 //            .strokeBorder(Color.blue, lineWidth: 40)
+//        VStack {
+//            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+////                .stroke(Color.red, lineWidth: 1)
+//                .fill(Color.red, style: FillStyle(eoFill: true))
+//
+//            Text("Offset")
+//            Slider(value: $petalOffset, in: -40...40)
+//                .padding([.horizontal, .bottom])
+//
+//            Text("Width")
+//            Slider(value: $petalWidth, in: 0...100)
+//                .padding(.horizontal)
+//
+//            Text("Hello World")
+//                .frame(width: 150, height: 150)
+////                .background(Color.red)
+////                .border(Color.red, width: 30)
+////                .background(Image("Example"))
+////                .border(Image("Example"), width: 30)
+////                .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
+//                .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+//
+//            Capsule()
+//                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
+//                .frame(width: 300, height: 200)
+//        }
         VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-//                .stroke(Color.red, lineWidth: 1)
-                .fill(Color.red, style: FillStyle(eoFill: true))
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
             
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-            
-            Text("Hello World")
-                .frame(width: 150, height: 150)
-//                .background(Color.red)
-//                .border(Color.red, width: 30)
-//                .background(Image("Example"))
-//                .border(Image("Example"), width: 30)
-//                .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
-                .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
-            
-            Capsule()
-                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
-                .frame(width: 300, height: 200)
+            Slider(value: $colorCycle)
         }
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+//                    .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+                .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                    self.color(for: value, brightness: 1),
+                    self.color(for: value, brightness: 0.5)
+                ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
