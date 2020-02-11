b7e4087fea22265e3a04ea0af865f1507cf8b6d0
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-11 20:20:04 +0200

    Try out some animation code

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -12,7 +12,10 @@ struct ContentView: View {
 //    @State private var petalOffset = -20.0
 //    @State private var petalWidth = 100.0
 //    @State private var colorCycle = 0.0
-    @State private var amount: CGFloat = 0.0
+//    @State private var amount: CGFloat = 0.0
+//    @State private var insetAmount: CGFloat = 50
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
 //        Path { path in
@@ -78,43 +81,116 @@ struct ContentView: View {
 //        .clipped()
 //        Image("aura")
 //            .colorMultiply(.red)
-        VStack {
-            ZStack {
-                Circle()
-//                    .fill(Color.red)
-                    .fill(Color(red: 1, green: 0, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-
-                Circle()
-//                    .fill(Color.green)
-                    .fill(Color(red: 0, green: 1, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-
-                Circle()
-//                    .fill(Color.blue)
-                    .fill(Color(red: 0, green: 0, blue: 1))
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
-            }
-            .frame(width: 300, height: 300)
-
-            Slider(value: $amount)
-                .padding()
-            
-            Image("aura")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .saturation(Double(amount))
-                .blur(radius: (1 - amount) * 20)
+//        VStack {
+//            ZStack {
+//                Circle()
+////                    .fill(Color.red)
+//                    .fill(Color(red: 1, green: 0, blue: 0))
+//                    .frame(width: 200 * amount)
+//                    .offset(x: -50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+////                    .fill(Color.green)
+//                    .fill(Color(red: 0, green: 1, blue: 0))
+//                    .frame(width: 200 * amount)
+//                    .offset(x: 50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+////                    .fill(Color.blue)
+//                    .fill(Color(red: 0, green: 0, blue: 1))
+//                    .frame(width: 200 * amount)
+//                    .blendMode(.screen)
+//            }
+//            .frame(width: 300, height: 300)
+//
+//            Slider(value: $amount)
+//                .padding()
+//
+//            Image("aura")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 200, height: 200)
+//                .saturation(Double(amount))
+//                .blur(radius: (1 - amount) * 20)
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(Color.black)
+//        .edgesIgnoringSafeArea(.all)
+//        Trapezoid(insetAmount: insetAmount)
+//            .frame(width: 200, height: 100)
+//            .onTapGesture {
+//                withAnimation {
+//                    self.insetAmount = CGFloat.random(in: 10...90)
+//                }
+//            }
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    self.rows = 8
+                    self.columns = 16
+                }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
-        .edgesIgnoringSafeArea(.all)
+        .drawingGroup()
+    }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+        
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        
+        return path
+    }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set {self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
     }
 }
 
